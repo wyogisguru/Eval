ff9f1e72e31b1c811661e2da32b6fc98c4646afa
cbrink <cbrink@cheyennebopu.org>
2015-11-24 11:43:26 -0700

    script complete

--- right_most_char.py
+++ right_most_char.py
@@ -6,7 +6,7 @@
 #
 # Author:      cbrink
 #
-# Created:     20/11/2015
+# Created:     24/11/2015
 # Copyright:   (c) cbrink 2015
 #-------------------------------------------------------------------------------
 import sys
@@ -28,7 +28,7 @@ def read_file(file=None):
             for char in line:
                 if char == ',':
                     t = line.index(char) + 1
-                    find_t1(t, line)
+                    find_t(t, line)
 
     except Exception, err:
         print('print_exc():')
@@ -36,44 +36,58 @@ def read_file(file=None):
 # ------------------------------------------------------------------------------
 def find_t(t=None,
            string=None):
-    S = string
-    tIndexes = []
-    for char in S:
-        if char == S[t]:
-            # Create list of occurences of t....
-            tIndexes.append(S.index(char))
-        else:
-            if char == ',':
-                break
+    try:
+        S = string
+        tIndexes = []
+        for char in S:
+            if char == S[t]:
+                # Create list of occurences of t....
+                tIndexes.append(S.index(char))
+            else:
+                if char == ',':
+                    break
 
-    # Pass 't' occurrences list to print function...
-    print_output(tIndexes)
+        # Pass 't' occurrences list to print function...
+        print_output(tIndexes)
+
+    except Exception, err:
+        print('print_exc():')
+        traceback.print_exc(file=sys.stdout)
 # ------------------------------------------------------------------------------
 def print_output(t_occurence=None):
-    # If no 't' was found print -1...
-    if len(t_occurence) == 0:
-        no_t = -1
-        print(no_t)
-    else:
-        if len(t_occurence) >= 1:
-            # Print right most occurence of 't' that is zero based...
-            rm_t = t_occurence[-1]
-            print(rm_t)
+    try:
+        # If no 't' was found print -1...
+        if len(t_occurence) == 0:
+            no_t = -1
+            print(no_t)
+        else:
+            if len(t_occurence) >= 1:
+                # Print right most occurence of 't' that is zero based...
+                rm_t = t_occurence[-1]
+                print(rm_t)
 
-    # Allow users to see printed output for 5 seconds...
+    except Exception, err:
+        print('print_exc():')
+        traceback.print_exc(file=sys.stdout)
 #-------------------------------------------------------------------------------
 def main():
-    filePath = str(input("Input file path including quotes and "
-                         "double backslashes: "
-                         "(i.e." r"'C:\\Temp\\your_text_file.txt')"))
+    try:
+        filePath = str(input("Input file path including quotes and "
+                             "double backslashes: "
+                             "(i.e." r"'C:\\Temp\\your_text_file.txt')"))
 
-    if filePath.isdigit():
-        print('Input cannot be just a number! '
-              '(Please run program again and input full path to text file.)')
-        time.sleep(5)
-    else:
-        read_file(filePath)
-        time.sleep(5)
+        if filePath.isdigit():
+            print('Input cannot be just a number! '
+                  '(Please run program again and input full '
+                  'path to text file.)')
+            time.sleep(5)
+        else:
+            read_file(filePath)
+            time.sleep(5)
+
+    except Exception, err:
+        print('print_exc():')
+        traceback.print_exc(file=sys.stdout)
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
